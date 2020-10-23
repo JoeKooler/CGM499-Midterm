6f360bcdc4a1b29bd318da4fb1c0a78f31fa4a7e
JoeKooler <jojesin@gmail.com>
2020-10-23 16:51:45 +0700

    Update from callback to async await

--- Assets/API/index.js
+++ Assets/API/index.js
@@ -1,118 +1,113 @@
-var socketIO = require('socket.io');
-const mongoose = require('mongoose');
+var socketIO = require("socket.io");
+const mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var io = socketIO(8888);
 
 const getRandomInt = (min, max) => {
-    var tempVal = 0;
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    tempVal = Math.floor(Math.random() * (max - min)) + min;    
-    console.log("Random Number = " + tempVal);
-    return tempVal;
-}
+  var tempVal = 0;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  tempVal = Math.floor(Math.random() * (max - min)) + min;
+  console.log("Random Number = " + tempVal);
+  return tempVal;
+};
 
+var randomNum = getRandomInt(1, 100);
 
-var randomNum = getRandomInt(1,100);
-
-mongoose.connect('mongodb://localhost:27017/unity', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/unity", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 var UserSchema = new Schema({
-    name: {type:String},
-    score: {type:Number}
+  name: { type: String },
+  score: { type: Number },
 });
 
-const UserModel = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model("User", UserSchema);
 
-//Server most likely to be online 24/7 mapping user and ip wouldn't be a good idea
-//We'll send userName from client everytime instead >_>
-//If we have login we'd do something like JWT anyway so it's not that heavy request
+const createUser = async (userName) => {
+  const isExist = await UserModel.findOne({ name: userName });
+  if (!isExist) {
+    const user = new UserModel({ name: userName, score: 0 });
+    await user.save();
+  }
+  console.log(`${userName} Connected!`);
+};
 
+const updateUserScore = async (userName, score) => {
+  const user = await UserModel.findOne({ name: userName });
+  user.score = score;
+  user.save();
+};
 
+const getUserScore = async (userName) => {
+  const user = await UserModel.findOne({ name: userName });
+  return user["score"];
+};
 
-const createUser = (userName,cb) => {
-    UserModel.findOne({ name: userName } , (err,user) => {
-        if(err)
-            throw(err);
-        if(user){
-            console.log("Already exist naja");
-            cb();
-        }else{
-            const user = new UserModel({ name: userName,score : 0 });
-            user.save().then(() => {
-                console.log("User has been created!");
-            }).then(() => cb());
-        }
-    })
-}
+const getTopFive = async () => {
+  const member = await UserModel.find({}).sort("-score").limit(5).exec();
+  return member;
+};
 
-
-const updateUserScore = (userName,score) => {
-    UserModel.findOne({"name" : userName}, (err,user) => {
-        if(err)
-            throw(err);
-        if(user){
-            user.score = score
-            user.save().then(() => console.log(`Update ${userName} score = ${score}`))
-        }
+const setUser = (socket) => {
+  socket.on("setUserName", async (data) => {
+    const userName = data["userName"];
+    await createUser(userName);
+    const userScore = await getUserScore(userName);
+    socket.emit("scoreUpdate", { score: userScore });
+    io.emit("server.reply", {
+      hint: userName + "Has entered the server",
+      isCorrect: false,
     });
-}
+  });
+};
 
-const getUserScore = (userName,cb) => {
-    UserModel.findOne({"name" : userName},(err,userInfo) => {
-        console.log("Score " + userInfo['score']);
-        cb(userInfo['score']);
-    });
-}
+const guess = (socket) => {
+  socket.on("guess", async (data) => {
+    const userName = data["userName"];
+    const guessedNumber = data["guessedNumber"];
+    if (guessedNumber < randomNum) {
+      socket.emit("server.reply", {
+        hint: "C'mon the answer is not that low...",
+        isCorrect: false,
+      });
+    } else if (guessedNumber > randomNum) {
+      socket.emit("server.reply", {
+        hint: "C'mon it's not that high :P",
+        isCorrect: false,
+      });
+    } else {
+      socket.emit("server.reply", {
+        hint: "Correct! you've got 1 point from that :D",
+        isCorrect: true,
+      });
+      io.emit("server.reply", {
+        hint: userName + " got the answer right! Re roll la~",
+        isCorrect: false,
+      });
+      const userScore = await getUserScore(userName);
+      updateUserScore(userName, userScore + 1);
+      randomNum = getRandomInt(1, 100);
+    }
+  });
+};
 
-const getTopFive = (cb) => {
-    UserModel.find({}).sort('-score').limit(5).exec((err,member) =>{
-        // console.log(member);
-        cb(member);
-    })
-}
+const getHighScore = (socket) => {
+  socket.on("gethighscore", async () => {
+    const member = await getTopFive();
+    socket.emit("postHighScore", { payload: member });
+    console.log(member);
+  });
+};
 
-io.on('connection',(socket) => {
-    console.log('client connected');
-    // console.log(`User IP: ${socket.conn.remoteAddress}`);
-
-    socket.on('setUserName',(data) => {
-        var userName = data['userName'];
-        createUser(userName,() => {
-            console.log(`${userName} Connected!`)
-            getUserScore(userName , (data) => {
-                // console.log(data);
-                socket.emit('scoreUpdate',{"score" : data});
-                io.emit('server.reply',{"hint": userName + " Has Entered the server","isCorrect" : false});
-            });
-        });
-    });
-
-    socket.on('guess',(data) => {
-        var userName = data['userName'];
-        var guessedNumber = data['guessedNumber'];
-        console.log(guessedNumber);
-        if(guessedNumber < randomNum){
-            socket.emit('server.reply',{"hint": "C'mon the answer is not that low...","isCorrect" : false});
-        }else if(guessedNumber > randomNum){
-            socket.emit('server.reply',{"hint": "C'mon it's not that high :P","isCorrect" : false});
-        }else{
-            socket.emit('server.reply',{"hint": "Correct! you've got 1 point from that :D","isCorrect" : true});
-            io.emit('server.reply',{"hint": userName + " got the answer right! Re roll la~","isCorrect" : false});
-            getUserScore(userName,(data) => {
-                updateUserScore(userName,data + 1)
-            })
-            randomNum = getRandomInt(1,100);
-        }
-    })
-
-    socket.on('gethighscore',() => {
-        getTopFive((member) => {
-            socket.emit('postHighScore',{"payload":member});
-            console.log(member);
-        });
-    })
+io.on("connection", (socket) => {
+  console.log("client connected");
+  setUser(socket);
+  guess(socket);
+  getHighScore(socket);
 });
 
-console.log('server started')
+console.log("server started");
